a2937e5d6db18fdcd7ef524b87cdf172f74efa08
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-17 10:24:53 +0100

    UF7_4.ej1 added

---
+++ src/dam/prog/actividadUF7_4/ej1_tetris/TetrisMain.java
@@ -0,0 +1,224 @@
+package dam.prog.actividadUF7_4.ej1_tetris;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+import dam.prog.actividadUF7_3.ej2.pojo.InvalidDataException;
+
+public class TetrisMain {
+
+	private static final boolean DEBUG = true;
+	
+	private static HashMap<String, Integer> scoreBoard;
+	
+	private static Scanner sc;
+	
+	private static String menu;
+	private static String[] menuOptions = {
+			"Salir del programa",
+			"Registrar puntuación",
+			"Mostrar puntuaciones"
+	};
+	
+	public static void main(String[] args) {
+		if (DEBUG) {
+			try {
+				sc = new Scanner(new File("src/dam/prog/actividadUF7_4/ej1_tetris/input.txt"));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		else
+			sc = new Scanner(System.in);
+		
+		scoreBoard = new HashMap<String, Integer>();
+
+		runTUI();
+	}
+	
+	private static void runTUI() {
+		boolean running = true;
+		menu = strJoin(menuOptions, "\n", "- %d ");
+		int selected;
+		while (running) {
+			selected = getIntInRange(
+				"Selecione una opción:\n" + menu + "\n-> ",
+				0, menuOptions.length - 1
+			);
+			System.out.println();
+			switch (selected) {
+			case 0:
+				running = false;
+				break;
+			case 1:
+				registraPuntuacion();
+				break;
+			case 2:
+				mostrarPuntuaciones();
+				break;
+			default:
+				throw new InvalidDataException("Ups, esto no tendría que pasar.");
+			}
+		}
+		System.out.println("Programa terminado");
+	}
+
+	private static void registraPuntuacion() {
+		System.out.println("Introduce los datos:");
+		String user = getString(" - Nombre -> ");
+		int puntuacion = getNatural(" - Puntuación -> ");
+		
+		if (scoreBoard.containsKey(user)) {
+			if (scoreBoard.get(user) < puntuacion) {
+				scoreBoard.put(user, puntuacion);
+				System.out.println("¡NUEVO RECORD!");
+			}
+			else {
+				System.out.println("¡PUNTUACIÓN NO SUPERADA!");
+			}
+		}
+		else {
+			scoreBoard.put(user, puntuacion);
+			System.out.println("¡PUNTUACIÓN ALMACENADA!");
+		}
+		System.out.println();
+	}
+	
+	private static void mostrarPuntuaciones() {
+		if (scoreBoard.size() == 0) {
+			System.out.println("La tabla está vacía :S\n");
+			return;
+		}
+		
+		TreeSet<String> usuarios = new TreeSet<String>();
+		
+		usuarios.addAll(scoreBoard.keySet());
+		
+		System.out.println("Listado de puntuaciones:");
+		for (String usr : usuarios) {
+			System.out.printf("%s: %d\n", usr, scoreBoard.get(usr));
+		}
+		System.out.println();
+	}
+
+	private static String strJoin(String[] arr, String sep, String prefix) {
+		String str = "";
+		if (prefix == null)
+			prefix = "";
+		if (arr.length == 0) {
+			return "";
+		}
+		
+		str += String.format(prefix + arr[0], 0);
+		for (int i = 1; i < arr.length; i++) {
+			str += String.format(sep + prefix + arr[i], i);
+		}
+		return str;
+	}
+	
+	// GETTERS
+	/**
+	 * @param question - Question to show using System.out
+	 * @param minLen - min length of String
+	 * @param maxLen - max length of String
+	 * @return Response given by the scanner meeting the criteria.
+	 */
+	public static String getString(String question, int minLen, int maxLen) {
+		String str;
+		while (true) {
+			str = getString(question);
+			if (str.length() < minLen) {
+				System.out.println("La longitud mínima es de " + minLen + " caracteres\n");
+			}
+			else if (str.length() > maxLen) {
+				System.out.println("La longitud máxima es de " + maxLen + " caracteres.\n");
+			}
+			else {
+				return str;
+			}
+		}
+	}
+	
+	public static String getString(String question) {
+		System.out.print(question);
+		String str = sc.nextLine();
+		if (DEBUG)
+			System.out.println(str); // TODO DEBUG
+		return str;
+	}
+
+	/**
+	 * @param question - Question to show using System.out.print
+	 * @return Integer given by Scanner
+	 */
+	public static int getInt(String question) {
+		String n;
+		while (true) {
+			try {
+				System.out.print(question);
+				n = sc.nextLine();
+				if (DEBUG)
+					System.out.println(n); // TODO DEBUG
+				return Integer.parseInt(n);
+			}
+			catch (NumberFormatException e) {
+				System.out.println("El valor no es un número entero válido.\n");
+			}
+		}
+	}
+	
+	/**
+	 * @param question - Question to show using System.out
+	 * @return Integer greater or equal to 0
+	 */
+	public static int getNatural(String question) {
+		int n = 0;
+		boolean isNotNatural = true;
+		while (isNotNatural) {
+			n = getInt(question);
+			
+			if (n >= 0) {
+				isNotNatural = false;
+			}
+			else {
+				System.out.println("El número tiene que ser un natural -> [0, inf)\n");
+			}
+		}
+		return n;
+	}
+	
+	/**
+	 * @param question - Question to show using System.out
+	 * @param min - min value of the desired int
+	 * @param max - max value of the desired int
+	 * @return Integer inside the interval [min, max]
+	 */
+	public static int getIntInRange(String question, int min, int max) {
+		if (min > max) {
+			int swap = min;
+			min = max;
+			max = swap;
+		}
+		
+		int n = 0;
+		boolean isNotValid = true;
+		while (isNotValid) {
+			n = getInt(question);
+			
+			if (n >= min && n <= max) {
+				isNotValid = false;
+			}
+			else {
+				System.out.printf(
+					"El número tiene que ser un natural en el rango [%d, %d]\n\n",
+					min, max
+				);
+			}
+		}
+		return n;
+	}
+}
